80e359724594bd41993655904e3226fef03a39b6
Dariush <59532348+lilTkar@users.noreply.github.com>
2020-03-01 18:42:20 +0330

    update date cmd and req file.

--- plugins/date.py
+++ plugins/date.py
@@ -1,20 +1,15 @@
 
-from pyrogram import Client , Filters , Message 
+from pyrogram import Client , Filters , Message
 import requests , time
-from bs4 import BeautifulSoup
+import jdatetime
 from db import r
 
 @Client.on_message(Filters.regex("^[Tt]oday$")& Filters.me)
 def today(app : Client , msg : Message):
-    req=requests.get("https://www.time.ir").content
-    clock = requests.get("http://gahshomar-api.herokuapp.com/zone/Asia-Tehran").text[:11]
 
-    soup     =BeautifulSoup(req,features="lxml")
-    data=soup.select(".panel-footer .normal ")
-    Gdate = data[0].text
-    Qdate = data[1].text
-    Sdate = data[2].text
-    text = f"┏ **Clock** : __{clock}__\n┣ **Shamsi** : __{Sdate}__\n┣ **Qamari** : __{Qdate}__\n┗ **Miladi** : __{Gdate}__\n**#Tkar**"
+    date = jdatetime.datetime.now().strftime('%d %b %Y')
+    clock = jdatetime.datetime.now().strftime('%H:%M:%S')
+    text = f"Clock : **{clock}**\nDate : **{date}**\n#Tkar"
     app.edit_message_text(
         chat_id=msg.chat.id,
         message_id=msg.message_id,
